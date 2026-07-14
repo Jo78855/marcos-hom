c915b0207c05286b026cd9435b2a5453d54f4a57
Jo78855 <joseph.sobhy2022@gmail.com>
2026-07-14 19:41:36 +0300

    Add Supabase admin dashboard

---
+++ supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://xusgwrcjdoueajuoesvp.supabase.co',
+  'sb_publishable_SnAJSXRi1YG3IC_igSeb0Q_CNd7xP8k',
+);
+
