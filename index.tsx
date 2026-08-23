088e0636c15b626d82b5bd6670cfda8fcc246f7b
Jo78855 <joseph.sobhy2022@gmail.com>
2026-08-23 14:11:34 +0300

    Load voice assistant styles

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import MarcosAssistant from './components/MarcosAssistant';
 import './styles.css';
+import './assistant.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
