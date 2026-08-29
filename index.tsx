32dcc9ab7d8a5995ff40af5bdfc682301b4a394e
Jo78855 <joseph.sobhy2022@gmail.com>
2026-08-29 22:00:24 +0300

    Remove broken standalone customer shop route

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import PublicOrder from './PublicOrder';
-import CustomerStorefront from './CustomerStorefront';
 import './styles.css';
 import './v2-extra.css';
 
@@ -11,5 +10,5 @@ if(!rootElement)throw new Error('Missing root element');
 const path=window.location.pathname;
 const customer=path.match(/^\/order\/customer\/([0-9a-f-]+)$/i);
 const technician=path.match(/^\/order\/technician\/([0-9a-f-]+)$/i);
-const screen=path.startsWith('/shop')?<CustomerStorefront/>:customer?<PublicOrder role="customer" token={customer[1]}/>:technician?<PublicOrder role="technician" token={technician[1]}/>:<App/>;
+const screen=customer?<PublicOrder role="customer" token={customer[1]}/>:technician?<PublicOrder role="technician" token={technician[1]}/>:<App/>;
 ReactDOM.createRoot(rootElement).render(<React.StrictMode>{screen}</React.StrictMode>);
