87e7d4b0e754748bb06434383564954745c3011e
Jo78855 <joseph.sobhy2022@gmail.com>
2026-08-29 21:12:58 +0300

    تفعيل صفحات العميل والفني العامة

--- index.tsx
+++ index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import PublicOrder from './PublicOrder';
 import './styles.css';
 import './v2-extra.css';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Missing root element');
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement=document.getElementById('root');
+if(!rootElement)throw new Error('Missing root element');
+const path=window.location.pathname;
+const customer=path.match(/^\/order\/customer\/([0-9a-f-]+)$/i);
+const technician=path.match(/^\/order\/technician\/([0-9a-f-]+)$/i);
+const screen=customer?<PublicOrder role="customer" token={customer[1]}/>:technician?<PublicOrder role="technician" token={technician[1]}/>:<App/>;
+ReactDOM.createRoot(rootElement).render(<React.StrictMode>{screen}</React.StrictMode>);
